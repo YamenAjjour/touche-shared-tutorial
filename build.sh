f0b17b45b9022633914cda68d9766fd01d954d6c
yamenajjour <yajjour@hotmail.com>
2020-01-07 08:38:23 +0100

    switching building to docker container

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ mkdir touche-shared-task-run
 %unzip input-dir/args-me.zip -d input-dir/
 wget https://raw.githubusercontent.com/webis-de/SIGIR-19/master/Data/topics.csv -O input-dir/topics.csv
 docker build -t my-docker -f Docker/Dockerfile .
-docker run -v "$(pwd)":/tmp/touche-shared-task -v "$(pwd)"/output-dir:/tmp/output-dir -v "$(pwd)"/input-dir:/tmp/input-dir -v touche-shared-task-run:/tmp/touche-shared-task-run -d --name my-docker
+docker run -v "$(pwd)":/tmp/touche-shared-task -v "$(pwd)"/output-dir:/tmp/output-dir -v "$(pwd)"/input-dir:/tmp/input-dir -v touche-shared-task-run:/tmp/touche-shared-task-run -d --name my-docker my-dcoker
 docker exec -it my-docker ./tmp/touche-shared-task/run.sh
