8a36d955ae6c612dbc6beb417b2d5b8f8dd79b9a
yamenajjour <yajjour@hotmail.com>
2020-01-07 09:05:07 +0000

    adding all files

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-./tmp/touche-shared-task/gradlew build --info
-java -jar /tmp/touche-shared-task/lib/build/touche-shared-task-1.0-SNAPSHOT-all.jar Main /tmp/input-dir/ /tmp/output-dir/
+cd /tmp/touche-shared-task/
+./gradlew build --info
+java -jar /tmp/touche-shared-task/build/libs/touche-shared-task-1.0-SNAPSHOT-all.jar Main /tmp/input-dir/ /tmp/output-dir/
