6aac25e9ac804a11769a48ee97af361b099a29b4
befi8957 <yamen.ajjour@uni-weimar.de>
2020-01-07 17:27:17 +0100

    finishing fixes

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 mkdir input-dir
 mkdir output-dir
 mkdir touche-shared-task-run
-%wget https://zenodo.org/record/3274636/files/argsme.zip?download=1 -O input-dir/args-me.zip
-%unzip input-dir/args-me.zip -d input-dir/
+wget https://zenodo.org/record/3274636/files/argsme.zip?download=1 -O input-dir/args-me.zip
+unzip input-dir/args-me.zip -d input-dir/
 wget https://raw.githubusercontent.com/webis-de/SIGIR-19/master/Data/topics.csv -O input-dir/topics.csv
 docker build -t my-docker-image:latest -f Docker/Dockerfile .
 docker run -dit --name my-docker-container -v "$(pwd)":/tmp/touche-shared-task -v "$(pwd)"/output-dir:/tmp/output-dir -v "$(pwd)"/input-dir:/tmp/input-dir -v touche-shared-task-run:/tmp/touche-shared-task-run  my-docker-image
