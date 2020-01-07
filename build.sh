c30089b5c1c09d1347fc02bdbfe999ed63052b91
yamenajjour <yajjour@hotmail.com>
2020-01-07 08:49:52 +0000

    adding files

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 mkdir input-dir
 mkdir output-dir
 mkdir touche-shared-task-run
@@ -8,5 +7,5 @@ mkdir touche-shared-task-run
 %unzip input-dir/args-me.zip -d input-dir/
 wget https://raw.githubusercontent.com/webis-de/SIGIR-19/master/Data/topics.csv -O input-dir/topics.csv
 docker build -t my-docker-image:latest -f Docker/Dockerfile .
-docker run -v "$(pwd)":/tmp/touche-shared-task --name my-docker-container -v "$(pwd)"/output-dir:/tmp/output-dir -v "$(pwd)"/input-dir:/tmp/input-dir -v touche-shared-task-run:/tmp/touche-shared-task-run -d my-docker-image
+docker run -dit --name my-docker-container -v "$(pwd)":/tmp/touche-shared-task -v "$(pwd)"/output-dir:/tmp/output-dir -v "$(pwd)"/input-dir:/tmp/input-dir -v touche-shared-task-run:/tmp/touche-shared-task-run  my-docker-image
 docker exec -it my-docker-container ./tmp/touche-shared-task/run.sh
