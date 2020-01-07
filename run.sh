5f42da66a3220153c3f32037f4ba1074aad1144b
yamenajjour <yajjour@hotmail.com>
2020-01-07 08:53:29 +0000

    fixing run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-RUN ./tmp/touche-shared-task/gradlew build --info
-RUN java -jar /tmp/touche-shared-task/lib/build/touche-shared-task-1.0-SNAPSHOT-all.jar Main /tmp/input-dir/ /tmp/output-dir/
+./tmp/touche-shared-task/gradlew build --info
+java -jar /tmp/touche-shared-task/lib/build/touche-shared-task-1.0-SNAPSHOT-all.jar Main /tmp/input-dir/ /tmp/output-dir/
