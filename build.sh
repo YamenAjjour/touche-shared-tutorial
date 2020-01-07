56ca15eaf32c66bef5f0d40eb95aa6ffcfa94944
yamenajjour <yajjour@hotmail.com>
2020-01-07 08:03:15 +0100

    switching building to docker container

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ wget https://zenodo.org/record/3274636/files/argsme.zip?download=1 -O input-dir/
 unzip input-dir/args-me.zip -d input-dir/
 wget https://raw.githubusercontent.com/webis-de/SIGIR-19/master/Data/topics.csv -O input-dir/topics.csv
 docker build -t my-docker -f Docker/Dockerfile .
-docker run -v "$(pwd)"/output-dir:/tmp/output-dir -v "$(pwd)"/input-dir:/tmp/input-dir -v touche-shared-task-run:/tmp/touche-shared-task-run -d -t my-docker
+docker run -v "$(pwd)":/tmp/touche-shared-task -v "$(pwd)"/output-dir:/tmp/output-dir -v "$(pwd)"/input-dir:/tmp/input-dir -v touche-shared-task-run:/tmp/touche-shared-task-run -d -t my-docker
