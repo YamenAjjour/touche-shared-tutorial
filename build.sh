952022f05e27eb3bf5df1e5109987738574eb846
Yamen Ajjour <yamen.ajjour@uni-weimar.de>
2020-01-09 14:40:45 +0100

    fixing run.sh problem

--- build.sh
+++ build.sh
@@ -15,4 +15,4 @@ fi
 wget https://raw.githubusercontent.com/webis-de/SIGIR-19/master/Data/topics.csv -O input-dir/topics.csv
 docker build -t my-docker-image:latest -f Docker/Dockerfile .
 docker run -dit --name my-docker-container -v "$(pwd)":/tmp/touche-shared-task -v "$(pwd)"/output-dir:/tmp/output-dir -v "$(pwd)"/input-dir:/tmp/input-dir -v touche-shared-task-run:/tmp/touche-shared-task-run  my-docker-image
-docker exec -it my-docker-container ./tmp/touche-shared-task/run.sh
+docker exec -it my-docker-container bash ./tmp/touche-shared-task/run.sh
