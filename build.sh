377e3077b499134a1d5bcecca3568bb7dbb17c26
Yamen Ajjour <yamen.ajjour@uni-weimar.de>
2020-01-09 14:35:04 +0100

    fixing a small bug

--- build.sh
+++ build.sh
@@ -3,8 +3,15 @@
 mkdir input-dir
 mkdir output-dir
 mkdir touche-shared-task-run
-wget https://zenodo.org/record/3274636/files/argsme.zip?download=1 -O input-dir/args-me.zip
-unzip input-dir/args-me.zip -d input-dir/
+args_me_corpus_zip=input-dir/args-me.zip
+args_me_corpus
+if test -f [!"args_me_corpus_zip"]; then
+    wget https://zenodo.org/record/3274636/files/argsme.zip?download=1 -O input-dir/args-me.zip
+fi
+if test -f [!"args_me_corpus"]; then
+    unzip input-dir/args-me.zip -d input-dir/
+fi
+
 wget https://raw.githubusercontent.com/webis-de/SIGIR-19/master/Data/topics.csv -O input-dir/topics.csv
 docker build -t my-docker-image:latest -f Docker/Dockerfile .
 docker run -dit --name my-docker-container -v "$(pwd)":/tmp/touche-shared-task -v "$(pwd)"/output-dir:/tmp/output-dir -v "$(pwd)"/input-dir:/tmp/input-dir -v touche-shared-task-run:/tmp/touche-shared-task-run  my-docker-image
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.logging.Handler;
@@ -8,6 +9,8 @@ import java.io.IOException;
 import java.util.List;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
+
+
 
 public class Main {
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
@@ -33,7 +36,9 @@ public class Main {
         //LOGGER.info("loading dataset");
         List<Argument> arguments = DatasetParser.loadDataset(inputPath);
         //LOGGER.info("indexing arguments");
-        SearchEngineSample.indexArguments(arguments);
+        if(Files.exists(Paths.get("/mnt/disk1/search-engine-sample/index"))) {
+            SearchEngineSample.indexArguments(arguments);
+        }
         IndexSearcher searcher = SearchEngineSample.loadSearcher();
         SearchResult[] results = new SearchResult[40];
         int i = 0;
