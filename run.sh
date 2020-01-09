0779cc2c7467b773d367e9377fb83e9327cc1889
Yamen Ajjour <yamen.ajjour@uni-weimar.de>
2020-01-09 15:20:54 +0100

    switching building to docker container

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 cd /tmp/touche-shared-task/
 ./gradlew build --info
-java -Xmx8g -Xms7g -jar /tmp/touche-shared-task/build/libs/touche-shared-task-1.0-SNAPSHOT-all.jar Main /tmp/input-dir/ /tmp/output-dir/
+java -Xmx8g -Xms7g -jar /tmp/touche-shared-task/build/libs/touche-shared-task-1.0-SNAPSHOT-all.jar Main "$0" "$1"
