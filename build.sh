9f06d911ffefcd03fecca14e486dee0645884b05
yamenajjour <yajjour@hotmail.com>
2020-01-19 22:47:53 +0100

    fixing issues

--- build.sh
+++ build.sh
@@ -16,5 +16,5 @@ fi
 
 wget https://raw.githubusercontent.com/webis-de/SIGIR-19/master/Data/topics.csv -O input-dir/topics.csv
 docker build -t my-docker-image:latest -f Docker/Dockerfile .
-docker run -dit --name my-docker-container -v "$(pwd)":/tmp/touche-shared-task my-docker-image
-docker exec -it my-docker-container bash ./tmp/touche-shared-task/run.sh "$1" "$2"
+docker run -dit --name my-docker-container -v "$(pwd)":/tmp/touche-shared-task -v "$1":input-dir -v "$2":output-dir my-docker-image
+docker exec -it my-docker-container bash ./tmp/touche-shared-task/run.sh input-dir output-dir
