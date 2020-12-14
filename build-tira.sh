5ba2cc03f8940f0d55b2eb3de31be064710646c8
yamen.ajjour@uni-weimar.de <yamen.ajjour@uni-weimar.de>
2020-12-14 17:02:57 +0100

    finishing tira zeug

--- build-tira.sh
+++ build-tira.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-mkdir input-dir
-mkdir output-dir
 docker build -t my-docker-image:latest -f Docker/Dockerfile .
-docker run -dit --name my-docker-container -v "$(pwd)":/tmp/touche-shared-task -v "$1":/tmp/input-dir -v "$2":/tmp/output-dir my-docker-image
-docker exec -it my-docker-container bash ./tmp/touche-shared-task/run.sh /tmp/input-dir /tmp/output-dir
+docker run --rm -ti --name my-docker-container -v "$1":/tmp/input-dir -v "$2":/tmp/output-dir my-docker-image
+
